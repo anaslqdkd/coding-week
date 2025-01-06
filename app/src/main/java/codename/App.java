2bd5ce284504e83bdbfdcd083be8f4f2ec3bf615
ingridgompel <ingrid.gompel@telecomnancy.eu>
2025-01-06 15:16:46 +0100

    Page de selection du mode : Mode Solo ou mode Multijoueurs

--- app/src/main/java/codename/App.java
+++ app/src/main/java/codename/App.java
@@ -10,12 +10,12 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        URL fxmlURL = getClass().getResource("/game.fxml");
+        URL fxmlURL = getClass().getResource("/selection_mode.fxml");
         if (fxmlURL == null) {
             System.err.println("Could not find game.fxml");
             System.exit(1);
         }
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/game.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/selection_mode.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setTitle("CodeName");
         primaryStage.setScene(scene);
