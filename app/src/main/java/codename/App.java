f710af8b5335c0f1fc431477f9ca3d86a765adcc
Raphaël PETRAU <raphael.petrau@telecomnancy.eu>
2025-01-09 09:50:55 +0100

    nettoyage du code pour settings

--- app/src/main/java/codename/App.java
+++ app/src/main/java/codename/App.java
@@ -1,11 +1,10 @@
 package codename;
 
-import codename.controller.MenuController;
-import codename.controller.SettingsController;
-import codename.model.Game;
-import codename.model.WordList;
 import java.net.URL;
 import java.util.List;
+
+import codename.model.Game;
+import codename.model.WordList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,72 +12,31 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
-  private Stage primaryStage;
+    private Game game; 
 
-  private Game game;
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    ; // Create a new game
-    List<String> words = WordList.getWordList(25, "database.txt");
-    this.game = Game.getInstance(words);
-    // this.game.setGridSize(6, 6);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        List<String> words = WordList.getWordList(25, "database.txt");
+        this.game = Game.getInstance(words);; // Create a new game
 
-    URL fxmlURL = getClass().getResource("/menu.fxml");
-    if (fxmlURL == null) {
-      System.err.println("Could not find menu.fxml");
-      System.exit(1);
+        URL fxmlURL = getClass().getResource("/menu.fxml");
+        if (fxmlURL == null) {
+            System.err.println("Could not find menu.fxml");
+            System.exit(1);
+        }
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/menu.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("CodeName");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-    FXMLLoader loader = new FXMLLoader(getClass().getResource("/menu.fxml"));
-    Parent root = loader.load();
 
-    Scene scene = new Scene(root);
-
-    primaryStage.setTitle("CodeName");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
-
-  public void showSettings() throws Exception {
-    URL fxmlURL = getClass().getResource("/settings.fxml");
-    if (fxmlURL == null) {
-      System.err.println("Could not find settings.fxml");
-      System.exit(1);
+    public static void main(String[] args) {
+        launch(args);
     }
-    FXMLLoader loader = new FXMLLoader(fxmlURL);
-    Parent root = loader.load();
-
-    // Pass the application reference to the controller
-    SettingsController controller = loader.getController();
-    controller.setApp(this);
-
-    Scene scene = new Scene(root);
-
-    primaryStage.setTitle("CodeName - Paramètres");
-    primaryStage.setScene(scene);
-  }
-
-  public void showMenu() throws Exception {
-    URL fxmlURL = getClass().getResource("/menu.fxml");
-    if (fxmlURL == null) {
-      System.err.println("Could not find menu.fxml");
-      System.exit(1);
-    }
-    FXMLLoader loader = new FXMLLoader(fxmlURL);
-    Parent root = loader.load();
-
-    // Pass the application reference to the controller
-    MenuController controller = loader.getController();
-    controller.setApp(this);
-
-    Scene scene = new Scene(root);
-
-    primaryStage.setTitle("CodeName - Menu");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
-
-  public static void main(String[] args) {
-    launch(args);
-  }
 }
+
--- app/src/main/java/codename/controller/SettingsController.java
+++ app/src/main/java/codename/controller/SettingsController.java
@@ -1,18 +1,11 @@
-/*
-Pour fonctionner, a besoin de :
 
-SettingsController controller = loader.getController();
-        GridController grid_controller = new GridController();
-        grid_controller.getWordList(25);
-
-*/
 
 package codename.controller;
 
-import codename.App;
+import java.io.IOException;
+
 import codename.Observer;
 import codename.model.Game;
-import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -34,43 +27,14 @@ public class SettingsController implements Observer {
 
   @FXML private CheckBox darkModeCheckbox;
 
-  @FXML private App app;
+
 
   @FXML private Button backButton;
+
   private Game game;
 
-  public void setApp(App app) {
-    this.app = app;
-  }
 
-  @FXML
-  private void goBackToMenu() {
-    try {
-      app.showMenu();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
 
-  // @FXML
-  // public void saveSettings() {
-  //    try {
-  //        System.out.println("Chargement de menu.fxml");
-  //        FXMLLoader loader = new FXMLLoader(getClass().getResource("/menu.fxml"));
-  //        Parent root = loader.load();
-  //
-  //        // Obtenir la scène actuelle
-  //        Stage stage = (Stage) saveSettingsButton.getScene().getWindow();
-  //        stage.setScene(new Scene(root));
-  //        stage.setTitle("Menu");
-  //        System.out.println("Navigation vers le menu réussie.");
-  //    } catch (Exception e) {
-  //        e.printStackTrace();
-  //        System.out.println("Erreur lors du chargement de la page menu.fxml");
-  //    }
-  //
-  //
-  // }
   @FXML
   private void initialize() {
     System.out.println("Settings controller initialised");
@@ -78,6 +42,7 @@ public class SettingsController implements Observer {
     System.out.println("Initial grid lines: " + gridLinesSlider.getValue());
     System.out.println("Initial grid columns: " + gridColumnsSlider.getValue());
     game.add_observer(this);
+
     int rows = (int) gridLinesSlider.getValue();
     int columns = (int) gridColumnsSlider.getValue();
     saveSettingsButton.setOnAction(
