4cea1378c920601be80640803f333dafb3d2416d
ash <ana.ceban@telecomnancy.eu>
2025-01-10 07:44:47 +0100

    rm fct inutile

--- app/src/main/java/codename/controller/GameController.java
+++ app/src/main/java/codename/controller/GameController.java
@@ -1,10 +1,7 @@
 package codename.controller;
 
 import codename.Observer;
-import codename.Timer;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 
 public class GameController implements Observer {
   @FXML private ClueAgentController clueAgentController; // Injecté automatiquement via fx:include
@@ -15,12 +12,6 @@ public class GameController implements Observer {
   @FXML private GridSpyController gridSpyController; // Injecté automatiquement via fx:include
 
   @FXML private InfoController infoController;
-  @FXML private Button startButton;
-  @FXML private Button stopButton;
-  private int seconds = 0;
-
-  @FXML private Label timerLabel;
-  private Timer gameTimer;
 
   public ClueAgentController getClueAgentController() {
     return clueAgentController;
@@ -44,39 +35,6 @@ public class GameController implements Observer {
 
   @FXML
   private void initialize() {}
-
-  public void startGameTimer() {
-    gameTimer.start(); // Start the timer when the game starts
-  }
-
-  public void stopGameTimer() {
-    gameTimer.stop(); // Stop the timer when the game ends
-  }
-
-  public void resetGameTimer() {
-    gameTimer.reset(); // Reset the timer if needed
-  }
-
-  private void updateTimer() {
-    seconds++;
-    int minutes = seconds / 60; // Calculate minutes
-    int remainingSeconds = seconds % 60; // Calculate remaining seconds
-
-    // Update the label with the formatted time (minutes:seconds)
-    timerLabel.setText(String.format("%02d:%02d", minutes, remainingSeconds));
-
-    System.out.println("Current Time: " + String.format("%02d:%02d", minutes, remainingSeconds));
-  }
-
-  @FXML
-  private void startButtonClicked() {
-    startGameTimer();
-  }
-
-  @FXML
-  private void stopButtonClicked() {
-    stopGameTimer();
-  }
 
   @Override
   public void update() {}
