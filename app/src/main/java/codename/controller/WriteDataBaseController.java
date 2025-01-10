6000bf11038c4dfa7218c08547c39301f68406e4
Raphaël PETRAU <raphael.petrau@telecomnancy.eu>
2025-01-10 11:40:25 +0100

    bouton quit depuis ecriture database

--- app/src/main/java/codename/controller/WriteDataBaseController.java
+++ app/src/main/java/codename/controller/WriteDataBaseController.java
@@ -1,12 +1,20 @@
 package codename.controller;
 
+import java.io.IOException;
+
+import codename.Observer;
+import codename.model.Game;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class WriteDataBaseController {
+public class WriteDataBaseController implements Observer {
 
     @FXML
     private TextArea databaseTextArea;
@@ -23,24 +31,32 @@ public class WriteDataBaseController {
     @FXML
     private Button saveButton;
 
+    private Game game;
+
     @FXML
     private void initialize() {
-        // Initialisation des composants si nécessaire
+        System.out.println("initialize WriteDataBaseController");
+        this.game = Game.getInstance();
+        game.add_observer(this);
+
+        cancelButton.setOnAction(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/settings.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = (Stage) cancelButton.getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
-    // @FXML
-    // private void handleSaveButtonAction() {
-    //     // Logique pour enregistrer la base de données
-    //     String title = titleField.getText();
-    //     String content = databaseTextArea.getText();
-    //     System.out.println("Enregistrer la base de données : " + title);
-    //     System.out.println("Contenu : " + content);
-    // }
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
 
-    // @FXML
-    // private void handleCancelButtonAction() {
-    //     // Logique pour annuler et fermer la fenêtre
-    //     Stage stage = (Stage) cancelButton.getScene().getWindow();
-    //     stage.close();
-    // }
+    }
 }
+
